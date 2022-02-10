6a81c200c76a36c3b311a92a0c582477dc209c49
simone <simone@darwintech.it>
2022-02-10 08:54:11 +0100

    reset repository

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,3 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::resource('pizza', \App\Http\Controllers\PizzeController::class);
-Route::post('pizzaMargherita', 'PizzeController@createMargherita');
