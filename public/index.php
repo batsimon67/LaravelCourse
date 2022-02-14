2a0b93895c3af85134f41629e73eef5bfce52030
simone <simone@darwintech.it>
2022-02-14 14:24:18 +0100

    fix index.php

--- public/index.php
+++ public/index.php
@@ -46,7 +46,7 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kesernel = $app->make(Kernel::class);
+$kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
     $request = Request::capture()
